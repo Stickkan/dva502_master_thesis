da668cbda7cdff0e9cef87610f93a60fb4be39a3
kootikatt <vp.96@hotmail.com>
2025-07-02 13:13:30 +0200

    added images and description of functionality

---
+++ Workspace/build/custom_drone_interfaces/ament_cmake_python/custom_drone_interfaces/custom_drone_interfaces/msg/_sub_waypoint_s.c
@@ -0,0 +1,205 @@
+// generated from rosidl_generator_py/resource/_idl_support.c.em
+// with input from custom_drone_interfaces:msg/SubWaypoint.idl
+// generated code does not contain a copyright notice
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#include <Python.h>
+#include <stdbool.h>
+#ifndef _WIN32
+# pragma GCC diagnostic push
+# pragma GCC diagnostic ignored "-Wunused-function"
+#endif
+#include "numpy/ndarrayobject.h"
+#ifndef _WIN32
+# pragma GCC diagnostic pop
+#endif
+#include "rosidl_runtime_c/visibility_control.h"
+#include "custom_drone_interfaces/msg/detail/sub_waypoint__struct.h"
+#include "custom_drone_interfaces/msg/detail/sub_waypoint__functions.h"
+
+ROSIDL_GENERATOR_C_IMPORT
+bool geometry_msgs__msg__point__convert_from_py(PyObject * _pymsg, void * _ros_message);
+ROSIDL_GENERATOR_C_IMPORT
+PyObject * geometry_msgs__msg__point__convert_to_py(void * raw_ros_message);
+ROSIDL_GENERATOR_C_IMPORT
+bool geometry_msgs__msg__vector3__convert_from_py(PyObject * _pymsg, void * _ros_message);
+ROSIDL_GENERATOR_C_IMPORT
+PyObject * geometry_msgs__msg__vector3__convert_to_py(void * raw_ros_message);
+ROSIDL_GENERATOR_C_IMPORT
+bool geometry_msgs__msg__vector3__convert_from_py(PyObject * _pymsg, void * _ros_message);
+ROSIDL_GENERATOR_C_IMPORT
+PyObject * geometry_msgs__msg__vector3__convert_to_py(void * raw_ros_message);
+
+ROSIDL_GENERATOR_C_EXPORT
+bool custom_drone_interfaces__msg__sub_waypoint__convert_from_py(PyObject * _pymsg, void * _ros_message)
+{
+  // check that the passed message is of the expected Python class
+  {
+    char full_classname_dest[54];
+    {
+      char * class_name = NULL;
+      char * module_name = NULL;
+      {
+        PyObject * class_attr = PyObject_GetAttrString(_pymsg, "__class__");
+        if (class_attr) {
+          PyObject * name_attr = PyObject_GetAttrString(class_attr, "__name__");
+          if (name_attr) {
+            class_name = (char *)PyUnicode_1BYTE_DATA(name_attr);
+            Py_DECREF(name_attr);
+          }
+          PyObject * module_attr = PyObject_GetAttrString(class_attr, "__module__");
+          if (module_attr) {
+            module_name = (char *)PyUnicode_1BYTE_DATA(module_attr);
+            Py_DECREF(module_attr);
+          }
+          Py_DECREF(class_attr);
+        }
+      }
+      if (!class_name || !module_name) {
+        return false;
+      }
+      snprintf(full_classname_dest, sizeof(full_classname_dest), "%s.%s", module_name, class_name);
+    }
+    assert(strncmp("custom_drone_interfaces.msg._sub_waypoint.SubWaypoint", full_classname_dest, 53) == 0);
+  }
+  custom_drone_interfaces__msg__SubWaypoint * ros_message = _ros_message;
+  {  // point
+    PyObject * field = PyObject_GetAttrString(_pymsg, "point");
+    if (!field) {
+      return false;
+    }
+    if (!geometry_msgs__msg__point__convert_from_py(field, &ros_message->point)) {
+      Py_DECREF(field);
+      return false;
+    }
+    Py_DECREF(field);
+  }
+  {  // velocity
+    PyObject * field = PyObject_GetAttrString(_pymsg, "velocity");
+    if (!field) {
+      return false;
+    }
+    if (!geometry_msgs__msg__vector3__convert_from_py(field, &ros_message->velocity)) {
+      Py_DECREF(field);
+      return false;
+    }
+    Py_DECREF(field);
+  }
+  {  // acceleration
+    PyObject * field = PyObject_GetAttrString(_pymsg, "acceleration");
+    if (!field) {
+      return false;
+    }
+    if (!geometry_msgs__msg__vector3__convert_from_py(field, &ros_message->acceleration)) {
+      Py_DECREF(field);
+      return false;
+    }
+    Py_DECREF(field);
+  }
+  {  // heading
+    PyObject * field = PyObject_GetAttrString(_pymsg, "heading");
+    if (!field) {
+      return false;
+    }
+    assert(PyFloat_Check(field));
+    ros_message->heading = PyFloat_AS_DOUBLE(field);
+    Py_DECREF(field);
+  }
+  {  // time_sec
+    PyObject * field = PyObject_GetAttrString(_pymsg, "time_sec");
+    if (!field) {
+      return false;
+    }
+    assert(PyFloat_Check(field));
+    ros_message->time_sec = PyFloat_AS_DOUBLE(field);
+    Py_DECREF(field);
+  }
+
+  return true;
+}
+
+ROSIDL_GENERATOR_C_EXPORT
+PyObject * custom_drone_interfaces__msg__sub_waypoint__convert_to_py(void * raw_ros_message)
+{
+  /* NOTE(esteve): Call constructor of SubWaypoint */
+  PyObject * _pymessage = NULL;
+  {
+    PyObject * pymessage_module = PyImport_ImportModule("custom_drone_interfaces.msg._sub_waypoint");
+    assert(pymessage_module);
+    PyObject * pymessage_class = PyObject_GetAttrString(pymessage_module, "SubWaypoint");
+    assert(pymessage_class);
+    Py_DECREF(pymessage_module);
+    _pymessage = PyObject_CallObject(pymessage_class, NULL);
+    Py_DECREF(pymessage_class);
+    if (!_pymessage) {
+      return NULL;
+    }
+  }
+  custom_drone_interfaces__msg__SubWaypoint * ros_message = (custom_drone_interfaces__msg__SubWaypoint *)raw_ros_message;
+  {  // point
+    PyObject * field = NULL;
+    field = geometry_msgs__msg__point__convert_to_py(&ros_message->point);
+    if (!field) {
+      return NULL;
+    }
+    {
+      int rc = PyObject_SetAttrString(_pymessage, "point", field);
+      Py_DECREF(field);
+      if (rc) {
+        return NULL;
+      }
+    }
+  }
+  {  // velocity
+    PyObject * field = NULL;
+    field = geometry_msgs__msg__vector3__convert_to_py(&ros_message->velocity);
+    if (!field) {
+      return NULL;
+    }
+    {
+      int rc = PyObject_SetAttrString(_pymessage, "velocity", field);
+      Py_DECREF(field);
+      if (rc) {
+        return NULL;
+      }
+    }
+  }
+  {  // acceleration
+    PyObject * field = NULL;
+    field = geometry_msgs__msg__vector3__convert_to_py(&ros_message->acceleration);
+    if (!field) {
+      return NULL;
+    }
+    {
+      int rc = PyObject_SetAttrString(_pymessage, "acceleration", field);
+      Py_DECREF(field);
+      if (rc) {
+        return NULL;
+      }
+    }
+  }
+  {  // heading
+    PyObject * field = NULL;
+    field = PyFloat_FromDouble(ros_message->heading);
+    {
+      int rc = PyObject_SetAttrString(_pymessage, "heading", field);
+      Py_DECREF(field);
+      if (rc) {
+        return NULL;
+      }
+    }
+  }
+  {  // time_sec
+    PyObject * field = NULL;
+    field = PyFloat_FromDouble(ros_message->time_sec);
+    {
+      int rc = PyObject_SetAttrString(_pymessage, "time_sec", field);
+      Py_DECREF(field);
+      if (rc) {
+        return NULL;
+      }
+    }
+  }
+
+  // ownership of _pymessage is transferred to the caller
+  return _pymessage;
+}
