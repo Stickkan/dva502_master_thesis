17c2e8ae5daa3b92896d091b5b37f7fb7f0ac156
kootikatt <vp.96@hotmail.com>
2025-07-04 16:40:30 +0200

    removed logs

--- Workspace/build/custom_drone_interfaces/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/viktor/dva502_master_thesis/Workspace/src/custom_drone_interfaces
-# Build directory: /home/viktor/dva502_master_thesis/Workspace/build/custom_drone_interfaces
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("custom_drone_interfaces__py")
--- Workspace/build/custom_drone_interfaces/rosidl_generator_py/custom_drone_interfaces/action/__init__.py
+++
@@ -1,2 +0,0 @@
-from custom_drone_interfaces.action._mission_executor import MissionExecutor  # noqa: F401
-from custom_drone_interfaces.action._tracker import Tracker  # noqa: F401
