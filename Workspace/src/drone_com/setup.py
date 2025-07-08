766325cca65d96a5b15dd45758dc47f36cca2371
Stickkan <fredrik.westerbom@outlook.com>
2025-07-08 14:25:28 +0200

    Now ros2 launch ... works. Changed some rows in package.xml and added setup.cfg

--- Workspace/src/drone_com/setup.py
+++ Workspace/src/drone_com/setup.py
@@ -1,26 +1,26 @@
-from setuptools import find_packages, setup
+#  GNU nano 6.2                                                                                       setup.py                                                                                                 
+from setuptools import setup
 
 package_name = 'drone_com'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='lennie',
-    maintainer_email='lennie_ce@hotmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='fredrik',
+    maintainer_email='lennie.ce@gmail.com',
+    description='Drone communication node',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'drone2drone = drone_com.drone2drone:main'
+            'drone2drone = drone_com.drone2drone:main',
         ],
     },
 )
